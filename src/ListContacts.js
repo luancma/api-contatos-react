e56f0c25e06348f629dd10a899640c8f5af01c93
tambal <luancma18@gmail.com>
2018-11-23 17:01:27 -0300

    comentandn partes importantes do código

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 
 class ListContacts extends Component {
 
+    // Componente controlado: 
+    //Componente que renderiza um formulário quando a fonte da verdade está presente no próprio componente.
+
     state = {
         query : ''
     }
@@ -13,30 +16,52 @@ class ListContacts extends Component {
     }
     render(){
 
+        //Destructor nos 'objetos' quey e contacts
         const { query } = this.state
         const { contacts } = this.props
 
-        return (
-            <div>
-                <div className='contact-top'>
-                    <input
-                        className='search-contacts'
-                        type='text'
-                        placeholder='Search Contacts'
-                        value={query}
-                        onChange={(event) => this.updateQuery(event.target.value)}
-                    />
-                </div>
-                <ol className='contact-list'>
-                    {contacts.map((contact) => (
-                        <li key={contact.id}>
-                            <img src={contact.avatarURL} height="60" width="60" />
-                            <p>{contact.name}</p>
-                        </li>
-                    ))}
-                </ol>
+        // Método que será esponsável 
+        const showContacts = query == '' 
+            ? contacts
+            : contacts.filter((c) => (
+                c.name.toLowerCase().includes(query.toLocaleLowerCase())
+            ))
+
+
+
+    return (
+        <div className='list-contacts'>
+            <div className='list-contacts-top'>
+          {/* Input onde receberá onde será mudado o valor da query, por meio do método updateQuery*/}
+                <input
+                className='search-contacts'
+                type='text'
+                placeholder='Search Contacts'
+                value={query}
+                onChange={(event) => this.updateQuery(event.target.value)}
+                />
             </div>
-        )
+        
+            {showContacts.length !== contacts.length && (
+                <div>
+                    Contatos diferente de show contact
+                </div>
+            )}
+
+          <ol className='contact-list'>
+          {/* Será mapeado novamente a cada nova entrada de valor que o input receber, pois o valor da'quety' está sendo atualizado */}
+            {showContacts.map((contact) => (
+              <li key={contact.id} className='contact-list-item'>
+                <div className='contact-details'>
+                  <p>{contact.name}</p>
+                  <p>{contact.handle}</p>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      )
     }
-}
+  }
+
 export default ListContacts;
